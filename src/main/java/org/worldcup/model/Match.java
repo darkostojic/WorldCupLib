c311ce4fdeb35412b58234c301f1f0722c9a1411
Darko Stojić <darko.stojic.93@gmail.com>
2025-02-22 17:06:04 +0100

    init models

---
+++ src/main/java/org/worldcup/model/Match.java
@@ -0,0 +1,61 @@
+package org.worldcup.model;
+
+import java.time.LocalDateTime;
+
+public class Match {
+
+    private String id;
+    private Team homeTeam;
+    private Team awayTeam;
+    private LocalDateTime matchStart;
+    private LocalDateTime matchFinish;
+
+    public Match() {
+    }
+
+    public Match(Team homeTeam, Team awayTeam, LocalDateTime matchStart) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.matchStart = matchStart;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Team getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(Team homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public Team getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(Team awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public LocalDateTime getMatchStart() {
+        return matchStart;
+    }
+
+    public void setMatchStart(LocalDateTime matchStart) {
+        this.matchStart = matchStart;
+    }
+
+    public LocalDateTime getMatchFinish() {
+        return matchFinish;
+    }
+
+    public void setMatchFinish(LocalDateTime matchFinish) {
+        this.matchFinish = matchFinish;
+    }
+}
---
+++ src/main/java/org/worldcup/model/Team.java
@@ -0,0 +1,31 @@
+package org.worldcup.model;
+
+public class Team {
+
+    private String name;
+    private Integer score;
+
+    public Team() {
+    }
+
+    public Team(String name, Integer score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+}
