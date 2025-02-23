dd1f6ac51fde9b4390b5a0ac5b11b5535fb4dc3c
Darko Stojić <darko.stojic.93@gmail.com>
2025-02-23 21:44:52 +0100

    implement InMemoryStorageService and WorldCupService

---
+++ src/main/java/org/worldcup/impl/InMemoryStorageServiceImpl.java
@@ -0,0 +1,46 @@
+package org.worldcup.impl;
+
+import org.worldcup.model.Match;
+import org.worldcup.model.MatchStatus;
+import org.worldcup.service.StorageService;
+import org.worldcup.storage.InMemoryStorage;
+
+import java.util.List;
+
+public class InMemoryStorageServiceImpl implements StorageService {
+
+    @Override
+    public String addNewMatch(Match match) {
+        return InMemoryStorage.addMatch(match);
+    }
+
+    @Override
+    public Match getMatch(String matchId) {
+        return InMemoryStorage.getMatch(matchId);
+    }
+
+    @Override
+    public void updateMatchScore(Match match, Integer homeTeamScore, Integer awayTeamScore) {
+        match.getHomeTeam().setScore(homeTeamScore);
+        match.getAwayTeam().setScore(awayTeamScore);
+    }
+
+    @Override
+    public List<Match> getAllLiveMatches() {
+        return InMemoryStorage.getAllMatches()
+                .values()
+                .stream()
+                .filter(match -> match.getMatchStatus().equals(MatchStatus.LIVE))
+                .toList();
+    }
+
+    @Override
+    public List<Match> getAllFinishedMatches() {
+        return InMemoryStorage.getAllMatches()
+                .values()
+                .stream()
+                .filter(match -> match.getMatchStatus().equals(MatchStatus.FINISHED))
+                .toList();
+    }
+
+}
---
+++ src/main/java/org/worldcup/impl/WorldCupServiceImpl.java
@@ -0,0 +1,64 @@
+package org.worldcup.impl;
+
+import org.worldcup.exception.NoMatchFoundException;
+import org.worldcup.model.Match;
+import org.worldcup.model.MatchStatus;
+import org.worldcup.model.Team;
+import org.worldcup.service.StorageService;
+import org.worldcup.service.WorldCupService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class WorldCupServiceImpl implements WorldCupService {
+
+    private final StorageService storageService;
+
+    public WorldCupServiceImpl() {
+        this.storageService = new InMemoryStorageServiceImpl();
+    }
+
+    @Override
+    public String startMatch(String homeTeamName, String awayTeamName) {
+        Team homeTeam = new Team(homeTeamName, 0);
+        Team awayTeam = new Team(awayTeamName, 0);
+
+        Match match = new Match(homeTeam, awayTeam, LocalDateTime.now(), MatchStatus.LIVE);
+        //TODO check if already started
+        return storageService.addNewMatch(match);
+    }
+
+    @Override
+    public void updateMatchScore(String matchId, Integer homeTeamScore, Integer awayTeamScore) throws NoMatchFoundException {
+        Match match = storageService.getMatch(matchId);
+        if(match == null || match.getMatchStatus().equals(MatchStatus.FINISHED)) {
+            throw new NoMatchFoundException("No live match found for id: " + matchId);
+        }
+        //TODO validate
+        storageService.updateMatchScore(match, homeTeamScore, awayTeamScore);
+    }
+
+    @Override
+    public void finishMatch(String matchId) throws NoMatchFoundException {
+        Match match = storageService.getMatch(matchId);
+        if(match == null || match.getMatchStatus().equals(MatchStatus.FINISHED)) {
+            throw new NoMatchFoundException("No live match found for id: " + matchId);
+        }
+        match.setMatchFinish(LocalDateTime.now());
+        match.setMatchStatus(MatchStatus.FINISHED);
+    }
+
+    @Override
+    public List<Match> getAllLiveMatches() {
+        List<Match> matches = storageService.getAllLiveMatches();
+        //TODO sort
+        return matches;
+    }
+
+    @Override
+    public List<Match> getAllFinishedMatches() {
+        List<Match> matches = storageService.getAllLiveMatches();
+        //TODO sort
+        return matches;
+    }
+}
