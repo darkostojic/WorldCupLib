9a382e9abd87b6946fcbcb7b9b8e06601d9aa02a
Darko Stojić <darko.stojic.93@gmail.com>
2025-02-23 23:29:30 +0100

    make sortMatchesByScoreSumAndStartTime return new list

--- src/main/java/org/worldcup/util/MatchSorter.java
+++ src/main/java/org/worldcup/util/MatchSorter.java
@@ -2,17 +2,20 @@ package org.worldcup.util;
 
 import org.worldcup.model.Match;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
 public class MatchSorter {
 
-    public static void sortMatchesByScoreSumAndStartTime(List<Match> matches) {
-        matches.sort(Comparator
-                .comparing((Match m) -> m.getHomeTeam().getScore() + m.getAwayTeam().getScore())
-                .reversed() // Descending order of score sum
-                .thenComparing(Match::getMatchStart) // Ascending order of matchStart
+    public static List<Match> sortMatchesByScoreSumAndStartTime(List<Match> matches) {
+        List<Match> sortedList = new ArrayList<>(matches);
+        sortedList.sort(Comparator
+                .comparingInt((Match m) -> m.getHomeTeam().getScore() + m.getAwayTeam().getScore()) // Sum of scores
+                .reversed()
+                .thenComparing(Match::getMatchStart, Comparator.reverseOrder())
         );
+        return sortedList;
     }
 
 }
